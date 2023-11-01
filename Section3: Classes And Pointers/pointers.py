67a2879732ea9252471a865b890b862a707adf67
knoori-code <knoori3@gmail.com>
2023-11-01 11:32:41 -0400

    Reassign num2 another number and check location in memory

--- Section3: Classes And Pointers/pointers.py
+++ Section3: Classes And Pointers/pointers.py
@@ -6,3 +6,12 @@ print('This is the value of num2:', num2)
 
 print('This is the location of num1:', id(num1))
 print('This is the location of num2:', id(num2))
+
+num2 = 23
+
+print('This is the value of num1:', num1)
+print('This is the value of num2:', num2)
+
+# num2 changes its location in memory from the original location
+print('This is the location of num1:', id(num1))
+print('This is the location of num2:', id(num2))
