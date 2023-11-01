89d6e057e27cc8c8237d7f73a1ed7f2c424673eb
knoori-code <knoori3@gmail.com>
2023-11-01 13:39:10 -0400

    Add comment

--- Section3: Classes And Pointers/pointers.py
+++ Section3: Classes And Pointers/pointers.py
@@ -4,6 +4,7 @@ num2 = num1
 print('This is the value of num1:', num1)
 print('This is the value of num2:', num2)
 
+# Shows the location in memory
 print('This is the location of num1:', id(num1))
 print('This is the location of num2:', id(num2))
 
