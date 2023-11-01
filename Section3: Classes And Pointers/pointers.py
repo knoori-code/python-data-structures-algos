8fb09d8acffd7fe43f6de1e35d6b9967ca0c173a
knoori-code <knoori3@gmail.com>
2023-11-01 11:29:35 -0400

    Create pointers.py file and show locations of variables in memory

---
+++ Section3: Classes And Pointers/pointers.py
@@ -0,0 +1,8 @@
+num1 = 11
+num2 = num1
+
+print('This is the value of num1:', num1)
+print('This is the value of num2:', num2)
+
+print('This is the location of num1:', id(num1))
+print('This is the location of num2:', id(num2))
