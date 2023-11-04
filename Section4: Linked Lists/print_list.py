088dd288cdf178e4cd0b9448ab9b36b4f750d6fc
knoori-code <knoori3@gmail.com>
2023-11-04 16:08:22 -0400

    Create method to print list

---
+++ Section4: Linked Lists/print_list.py
@@ -0,0 +1,22 @@
+# Method to get the values of each item in the linked list
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1 
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+new_linked_list = LinkedList(1)
+
+new_linked_list.print_list()
