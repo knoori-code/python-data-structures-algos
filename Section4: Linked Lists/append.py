aa113c938b1ab26401d7bed364e2657a3ce0d0db
knoori-code <knoori3@gmail.com>
2023-11-04 16:47:03 -0400

    Append item to list and print all items in list

--- Section4: Linked Lists/append.py
+++ Section4: Linked Lists/append.py
@@ -23,7 +23,14 @@ class LinkedList:
             self.head = new_node
             self.tail = new_node
         else:
-            self.tale.next = new_node
+            self.tail.next = new_node
             self.tail = new_node
         self.length += 1
         return True
+    
+
+new_linked_list = LinkedList(1)
+
+new_linked_list.append(2)
+
+new_linked_list.print_list()
