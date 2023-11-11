63638e83fa4a89731b7c6b2c4ca6a4311946d3c1
knoori-code <knoori3@gmail.com>
2023-11-11 15:00:16 -0500

    Complete code for enqueue method

--- Section10: Stack and Queues/queue_enqueue.py
+++ Section10: Stack and Queues/queue_enqueue.py
@@ -18,4 +18,12 @@ class Queue:
             temp = temp.next
 
     def enqueue(self, value):
-        
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+        return True
