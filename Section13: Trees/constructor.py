ba447b9cee98550ce2309a8dee69011b7682dcff
knoori-code <knoori3@gmail.com>
2023-11-16 21:32:03 -0500

    Write code for constructor and initial node

---
+++ Section13: Trees/constructor.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+
+new_tree = BinarySearchTree()
+
+print(new_tree.root)
