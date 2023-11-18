1136b193b1fecb18a09eb9bef437a7dd498fdd62
knoori-code <knoori3@gmail.com>
2023-11-18 18:39:20 -0500

    Check code by inserting new nodes

--- Section13: Trees/insert.py
+++ Section13: Trees/insert.py
@@ -20,7 +20,7 @@ class BinarySearchTree:
                 return False
             if new_node.value < temp.value:
                 if temp.left is None:
-                    temp.left == new_node
+                    temp.left = new_node
                     return True
                 temp = temp.left
             else:
@@ -28,3 +28,15 @@ class BinarySearchTree:
                     temp.right = new_node
                     return True
                 temp = temp.right         
+
+
+new_tree = BinarySearchTree()
+
+
+new_tree.insert(2)
+new_tree.insert(1)
+new_tree.insert(3)
+
+print(new_tree.root.value)
+print(new_tree.root.left.value)
+print(new_tree.root.right.value)
