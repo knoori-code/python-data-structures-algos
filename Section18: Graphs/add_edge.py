db25768376ca3645f5ada1ce721d38907650fa0a
knoori-code <knoori3@gmail.com>
2023-12-13 18:50:31 -0500

    Complete code for add edge method

--- Section18: Graphs/add_edge.py
+++ Section18: Graphs/add_edge.py
@@ -12,4 +12,9 @@ class Graph:
             return True
         return False
     
-    def add_edge(self):
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
